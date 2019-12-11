c2637049c9a9d212e722749c62b35d834414fb61
Morten Poul Jensen <mpj@signifly.com>
2019-12-11 11:57:39 +0100

    wip

---
+++ WithWebsockets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Signifly\Travy\Schema\Concerns;
+
+interface WithWebsockets
+{
+    public function websockets(): string;
+}
--- src/Config.php
+++ src/Config.php
@@ -2,10 +2,13 @@
 
 namespace Signifly\Travy\Schema;
 
+use Signifly\Travy\Schema\Concerns\AppliesConcerns;
 use Signifly\Travy\Schema\Contracts\Config as Contract;
 
 abstract class Config extends Definition implements Contract
 {
+    use AppliesConcerns;
+
     public function toArray()
     {
         $schema = Schema::make([
@@ -16,8 +19,11 @@ abstract class Config extends Definition implements Contract
             'title' => $this->title(),
         ]);
 
-        if (isset($this->websocketUrl)) {
-            $schema->set('websockets', $this->websocketUrl);
+        $this->applyConcerns($schema);
+
+        // Allow doing some final configurations
+        if (method_exists($this, 'prepareSchema')) {
+            $this->prepareSchema($schema);
         }
 
         return $schema->toArray();
