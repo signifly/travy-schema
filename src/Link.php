b1d08d9c892ec07ba3e21904d728c59f7e635e70
Morten Poul Jensen <mpj@signifly.com>
2020-03-20 14:17:12 +0100

    Add external method to link

--- src/Link.php
+++ src/Link.php
@@ -9,6 +9,13 @@ class Link extends Action
     use HasEndpoint;
 
     /**
+     * Whether the link is external.
+     *
+     * @var bool
+     */
+    protected $external = false;
+
+    /**
      * The URL to link to.
      *
      * @var string
@@ -26,8 +33,22 @@ class Link extends Action
             'id' => 'link',
             'props' => [
                 'url' => $this->url,
+                'external' => $this->external,
             ],
         ];
+    }
+
+    /**
+     * Set the external property.
+     *
+     * @param  bool $value
+     * @return self
+     */
+    public function external(bool $value = true): self
+    {
+        $this->external = $value;
+
+        return $this;
     }
 
     /**
